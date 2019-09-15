2da5fc4d0d477f0012afa78870ba5d926439794b
Generic Math Student <mathstudent@mathsoc.ums-ubc.com>
2019-09-14 19:23:54 -0700

    first commit

---
+++ make_lists.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <pqxx/pqxx> 
+
+using namespace std;
+using namespace pqxx;
+
+int main(int argc, char* argv[]) {
+   char * sql;
+   
+   try {
+      connection C("dbname = rateme user = rateme password = 1 \
+      hostaddr = 127.0.0.1 port = 5432");
+      if (C.is_open()) {
+         cout << "Opened database successfully: " << C.dbname() << endl;
+      } else {
+         cout << "Can't open database" << endl;
+         return 1;
+      }
+
+      /* Create SQL statement */
+      sql = "SELECT * from rateme_rating";
+
+      /* Create a non-transactional object. */
+      nontransaction N(C);
+      
+      /* Execute SQL query */
+      result R( N.exec( sql ));
+      
+      /* List down all the records */
+      for (result::const_iterator c = R.begin(); c != R.end(); ++c) {
+         cout << c[0].as<int>() << " " << c[1].as<int>() << " is " << c[2].as<string>() << " in " << c[3].as<int>() << endl;
+      }
+      cout << "Operation done successfully" << endl;
+      C.disconnect ();
+   } catch (const std::exception &e) {
+      cerr << e.what() << std::endl;
+      return 1;
+   }
+
+   return 0;
+}
